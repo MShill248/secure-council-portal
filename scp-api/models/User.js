e9a2da8d122d99fcd416830f869954e22f93974c
ali <alichocr2@gmail.com>
2025-09-16 12:16:27 +0100

    apply fixes to User model

--- scp-api/models/User.js
+++ scp-api/models/User.js
@@ -2,16 +2,24 @@ const db = require('../database/connect');
 
 class User {
 
-    constructor({ user_id, username, password, high_score = 0, is_admin }) {
-        this.id = user_id;
+    constructor({ user_id,  username, first_name, last_name, email, password, dob, address, postcode, borough, phone_number, user_role}) {
+        this.user_id = user_id;
         this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
         this.password = password;
-        this.high_score = high_score;
-        this.isAdmin = is_admin;
+        this.dob = dob;
+        this.address = address;
+        this.postcode = postcode;
+        this.borough = borough;
+        this.phone_number = phone_number;
+        this.user_role = user_role;
+
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT * FROM user_account WHERE user_id = $1", [id]);
+        const response = await db.query("SELECT * FROM users WHERE user_id = $1", [id]);
         if (response.rows.length != 1) {
             throw new Error("Unable to locate user.");
         }
@@ -19,7 +27,7 @@ class User {
     }
 
     static async getOneByUsername(username) {
-        const response = await db.query("SELECT * FROM user_account WHERE username = $1", [username]);
+        const response = await db.query("SELECT * FROM users WHERE username = $1", [username]);
         if (response.rows.length != 1) {
             throw new Error("Unable to locate user.");
         }
@@ -27,9 +35,9 @@ class User {
     }
 
     static async create(data) {
-        const { username, password, isAdmin } = data;
-        let response = await db.query("INSERT INTO user_account (username, password, high_score) VALUES ($1, $2, 0) RETURNING user_id;",
-            [username, password]);
+        const {username, first_name, last_name, email, password, dob, address, postcode, borough, phone_number, user_role} = data;
+        let response = await db.query("INSERT INTO users (username, first_name, last_name, email, password, dob, address, postcode, borough, phone_number, user_role) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING user_id;",
+            [username, first_name, last_name, email, password, dob, address, postcode, borough, phone_number, user_role]);
         const newId = response.rows[0].user_id;
         const newUser = await User.getOneById(newId);
         return newUser;
@@ -38,15 +46,15 @@ class User {
     async update(data){
         const { username, first_name, last_name, email, password, dob, address, postcode, borough, phone_number } = data
 
-        const response = await db.query("UPDATE userinfo SET username = COALESCE($1, username) first_name = COALESCE($2, first_name), last_name = COALESCE($3, last_name), email = COALESCE($4, email), password = COALESCE($5, passwordhash), dob = COALESCE($6, dob), address = COALESCE($7, address), postcode = COALESCE($8, postcode), borough = COALESCE($9, borough), phone_number = COALESCE($10, phone_number) WHERE user_id = $11 RETURNING *;", [username, first_name, last_name, email, password, dob, address, postcode, borough, phone_number, this.user_id])
+        const response = await db.query("UPDATE users SET username = COALESCE($1, username) first_name = COALESCE($2, first_name), last_name = COALESCE($3, last_name), email = COALESCE($4, email), password = COALESCE($5, passwordhash), dob = COALESCE($6, dob), address = COALESCE($7, address), postcode = COALESCE($8, postcode), borough = COALESCE($9, borough), phone_number = COALESCE($10, phone_number) WHERE user_id = $11 RETURNING *;", [username, first_name, last_name, email, password, dob, address, postcode, borough, phone_number, this.user_id])
         if (response.rows.length !== 1) {
             throw Error("Unable to update user")
         }
-        return new UserInfo(response.rows[0]) 
+        return new User(response.rows[0]) 
     }
 
     async destroy() {
-        const response = await db.query("DELETE FROM userinfo WHERE userid = $1;", [this.userid])
+        const response = await db.query("DELETE FROM users WHERE user_id = $1;", [this.user_id])
     }
 }
 
