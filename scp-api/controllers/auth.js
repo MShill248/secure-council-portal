aa2a72e20571af329aa0a0deb1c92c5fe7c3f1b2
ali <alichocr2@gmail.com>
2025-09-16 15:35:44 +0100

    add functionality for login + 2FA

--- scp-api/controllers/auth.js
+++ scp-api/controllers/auth.js
@@ -21,7 +21,7 @@ async function register(req, res) {
 async function login(req, res) {
     try {
         const data = req.body;
-        const user = await User.getOneByEmail(data.email);
+        const user = await User.getOneByUsername(data.username);
         if (!user) {
             return res.status(404).json({ error: "User not found" });
         }
@@ -29,12 +29,12 @@ async function login(req, res) {
         if (!match) {
             return res.status(401).json({ error: "Invalid credentials" });
         }
-        if (user.userrole.toLowerCase() == 'developer') {
+        if (user.user_role.toLowerCase() == 'developer') {
             // Bypass MFA for developer accounts
             const payload = { email: user.email };
             jwt.sign(payload, process.env.SECRET_TOKEN, { expiresIn: 3600 }, (err, token) => {
                 if (err)return res.status(500).json({ error: 'Error in token generation' });
-                res.status(200).json({ success: true, token, userid: user.userid, userrole: user.userrole});
+                res.status(200).json({ success: true, token, user_id: user.user_id, user_role: user.user_role});
             });
         } else {
             const otp = (Math.floor(100000 + Math.random() * 900000)).toString();
