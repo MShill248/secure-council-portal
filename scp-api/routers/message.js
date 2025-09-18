679ec7f2d9ee4b346c7b12e2fb001c6cd3a09428
ali <alichocr2@gmail.com>
2025-09-18 16:59:29 +0100

    ammend routers for request and message to use authenticator

--- scp-api/routers/message.js
+++ scp-api/routers/message.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const messageController = require('../controllers/message.js');
+const authenticator = require('../middleware/authenticator.js');
 //const authenticator = require("../middleware/authenticator")
 
 const messageRouter = Router()
@@ -9,8 +10,8 @@ const messageRouter = Router()
 messageRouter.get('/', messageController.index)
 messageRouter.get('/request/:request_id', messageController.getByRequestId)
 messageRouter.get('/:id', messageController.showId)
-messageRouter.post("/", messageController.create)
-messageRouter.patch("/:id", messageController.update)
-messageRouter.delete("/:id", messageController.destroy)
+messageRouter.post("/", authenticator, messageController.create)
+messageRouter.patch("/:id", authenticator, messageController.update)
+messageRouter.delete("/:id", authenticator, messageController.destroy)
 
 module.exports = messageRouter;
