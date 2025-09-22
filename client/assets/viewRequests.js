33e4ed729c150045ca787851bbae572ad828b045
tfs18 <tfitzsmith1@gmail.com>
2025-09-22 12:05:41 +0100

    add functionality for resident and council to view/edit requests

--- client/assets/viewRequests.js
+++ client/assets/viewRequests.js
@@ -35,6 +35,16 @@ document.addEventListener("DOMContentLoaded", async () => {
         residentPhone.value = user.phone_number
         requestDescription.textContent = request.description
 
+        const messageResponse = await fetch(`http://localhost:3000/message/request/${requestId}`, options)
+        console.log(messageResponse);
+        if (messageResponse.ok) {
+            const messageData = await messageResponse.json()
+            console.log(messageData);
+            councilMessage.value = messageData[0].content || ""
+        } else {
+            console.error("Failed to fetch council message:", messageResponse.statusText)
+        }
+
     } catch (error) {
         console.error("Error loading request details:", error)
         alert("Could not load request details.")
